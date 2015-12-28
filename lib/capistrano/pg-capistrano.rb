72c76b0fb022d85253eee20c405759c2e0738329
Marcos Fadul <marcos@polargold.de>
2015-12-28 16:41:00 +0100

    Minor: add include for typo3 tasks

--- lib/capistrano/pg-capistrano.rb
+++ lib/capistrano/pg-capistrano.rb
@@ -1,3 +1,4 @@
 require "capistrano"
 require File.expand_path("#{File.dirname(__FILE__)}/pg-capistrano/none")
+require File.expand_path("#{File.dirname(__FILE__)}/pg-capistrano/typo3")
 require File.expand_path("#{File.dirname(__FILE__)}/db-tasks/dbtasks")
--- lib/capistrano/pg-capistrano/typo3.rb
+++ lib/capistrano/pg-capistrano/typo3.rb
@@ -1,7 +1,7 @@
 namespace :typo3 do
   desc "clearCache"
   task :clearCache do
-    on roles(:all) do
+    on roles(:web) do
       typo3_cli = "#{fetch(:current_path)}/web/typo3/cli_dispatch.phpsh"
       within release_path do
         execute :php, typo3_cli, "extbase cacheapi:clearallcaches"
