0ff99ac6da1c8cf60a7a6a51feefbcc634addbac
Marcos Fadul <marcos@polargold.de>
2015-12-28 16:57:30 +0100

    Minor: Fix typo3_cli path

--- lib/capistrano/pg-capistrano/typo3.rb
+++ lib/capistrano/pg-capistrano/typo3.rb
@@ -2,7 +2,7 @@ namespace :typo3 do
   desc "clearCache"
   task :clearCache do
     on roles(:web) do
-      typo3_cli = "#{fetch(:release_path).join('web/typo3/cli_dispatch.phpsh')}"
+      typo3_cli = [fetch(:release_path)].join('web/typo3/cli_dispatch.phpsh')
       within release_path do
         execute :php, typo3_cli, "extbase cacheapi:clearallcaches"
       end
